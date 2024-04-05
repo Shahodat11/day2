358c1ba4b96bbef8ff752d58cfa23ca61e013a6f
unknown <shahodatusmonjonova@gmail.com>
2024-04-06 00:32:47 +0500

    “categories”

--- src/components/categories/Categories.js
+++ src/components/categories/Categories.js
@@ -2,6 +2,11 @@ import React from 'react'
 import '../categories/categories.css'
 import categories from '../images/Frame 623 (1).svg'
 import { IoPhonePortraitOutline } from "react-icons/io5";
+import { HiOutlineComputerDesktop } from "react-icons/hi2";
+import { BsSmartwatch } from "react-icons/bs";
+import { CiCamera } from "react-icons/ci";
+import { CiHeadphones } from "react-icons/ci";
+import { IoGameControllerOutline } from "react-icons/io5";
 
 function Categories() {
   return (
@@ -14,8 +19,28 @@ function Categories() {
         </div>
         <div className="nav5">
           <div className="nav6">
-            <IoPhonePortraitOutline />
-            <h5>Phones</h5>
+            <IoPhonePortraitOutline className="img6" />            
+            <h5 className='h5'>Phones</h5>
+          </div>
+          <div className="nav6">
+            <HiOutlineComputerDesktop className="img6" />            
+            <h5 className='h5'>Computers</h5>
+          </div>
+          <div className="nav6">
+            <BsSmartwatch className="img6" />            
+            <h5 className='h5'>SmartWatch</h5>
+          </div>
+          <div className="nav6">
+            <CiCamera className="img6" />            
+            <h5 className='h5'>Camera</h5>
+          </div>
+          <div className="nav6">
+            <CiHeadphones className="img6" />            
+            <h5 className='h5'>HeadPhones</h5>
+          </div>
+          <div className="nav6">
+            <IoGameControllerOutline className="img6" />            
+            <h5 className='h5'>Gaming</h5>
           </div>
         </div>
       </div>    
